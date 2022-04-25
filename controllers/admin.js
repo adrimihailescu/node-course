223c6289d95b08b01e8d44e2e3e658c9f1d5add4
adriana <adriana.mihalescu@gmail.com>
2022-04-25 17:41:03 +0100

    working on navidation and registering the routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,7 @@ exports.getAddProduct = (req, res, next) => {
 	// console.log("In another middleware!");
 	res.render("admin/add-product", {
 		pageTitle: "Add Product",
-		path: "admin/add-product",
+		path: "/admin/add-product",
 		formsCSS: true,
 		productCSS: true,
 		activeAddProduct: true,
@@ -20,14 +20,11 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-	const products = Product.fetchAll((products) => {
-		res.render("shop/product-list", {
+	Product.fetchAll((products) => {
+		res.render("admin/products", {
 			prods: products,
-			pageTitle: "Shop",
-			path: "/",
-			hasProducts: products.length > 0,
-			activeShop: true,
-			productCSS: true,
+			pageTitle: "Admin Products",
+			path: "/admin/products",
 		});
 	});
 };
