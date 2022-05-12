177dd069c48bafd3b7388b0ed9503d0fc7cb0b6d
adriana <adriana.mihalescu@gmail.com>
2022-05-12 14:15:44 +0100

    redefining the product model with sequelize

--- models/product.js
+++ models/product.js
@@ -1,86 +1,27 @@
-const db = require("../util/database");
+const Sequelize = require("sequelize");
 
-// ALL COMMENTED LINES ARE FOR THE OPTION OF SAVING DATA TO FILE
-// const products = [];
-// const fs = require("fs");
-// const path = require("path");
+const sequelize = require("../util/database");
 
-const Cart = require("./cart");
+const Product = sequelize.define("product", {
+	id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primaryKey: true,
+	},
+	title: Sequelize.STRING,
+	price: {
+		type: Sequelize.DOUBLE,
+		allowNull: false,
+	},
+	imageUrl: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	description: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+});
 
-// const p = path.join(
-// 	path.dirname(process.mainModule.filename),
-// 	"data",
-// 	"products.json"
-// );
-
-//helper function          cb= callback function
-// const getProductsFromFile = (cb) => {
-// 	fs.readFile(p, (err, fileContent) => {
-// 		if (err) {
-// 			cb([]);
-// 		} else {
-// 			cb(JSON.parse(fileContent));
-// 		}
-// 	});
-// };
-
-module.exports = class Product {
-	constructor(id, title, imageUrl, description, price) {
-		this.id = id;
-		this.title = title;
-		this.imageUrl = imageUrl;
-		this.description = description;
-		this.price = price;
-	}
-	// method available in this class
-	//function for adding new products or editing existing products
-	save() {
-		return db.execute(
-			"INSERT INTO products (title, price, imageUrl, description)  VALUE (?, ?, ?, ?)",
-			[this.title, this.price, this.imageUrl, this.description]
-		);
-		// getProductsFromFile((products) => {
-		// 	if (this.id) {
-		// 		const existingProductIndex = products.findIndex(
-		// 			(prod) => prod.id === this.id
-		// 		);
-		// 		const updatedProducts = [...products];
-		// 		updatedProducts[existingProductIndex] = this;
-		// 		fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-		// 			console.log(err);
-		// 		});
-		// 	} else {
-		// 		this.id = Math.random().toString();
-		// 		products.push(this);
-		// 		fs.writeFile(p, JSON.stringify(products), (err) => {
-		// 			console.log(err);
-		// 		});
-		// 	}
-		// });
-	}
-
-	static deleteById(id) {
-		// getProductsFromFile((products) => {
-		// 	const product = products.find((prod) => prod.id === id);
-		// 	const updatedProducts = products.filter((prod) => prod.id !== id);
-		// 	fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-		// 		if (!err) {
-		// 			Cart.deleteProduct(id, product.price);
-		// 		}
-		// 	});
-		// });
-	}
-	//retrieve all products from the array, method that calls directly on the class itself
-	static fetchAll(cb) {
-		return db.execute("SELECT * FROM products");
-		// getProductsFromFile(cb);
-	}
-
-	static findById(id, cb) {
-		return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
-		// getProductsFromFile((products) => {
-		// 	const product = products.find((p) => p.id === id);
-		// 	cb(product);
-		// });
-	}
-};
+module.exports = Product;
