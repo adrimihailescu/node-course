0b30a13d2a6ed356508a723a6ff4f75ca3c095bf
adriana <adriana.mihalescu@gmail.com>
2022-05-12 14:22:28 +0100

    add syncing js definitions to the database

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 // const expressHbs = require("express-handlebars");
 
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -40,5 +40,15 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequelize
+	.sync()
+	.then((result) => {
+		// console.log(result);
+		app.listen(3000);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 app.listen(3000);
