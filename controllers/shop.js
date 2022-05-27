1dbc1596151c63411451c34f31ede997cff77588
adriana <adriana.mihalescu@gmail.com>
2022-05-27 17:12:55 +0100

    add functionality for creating orders and getting them

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product");
 // const Cart = require("../models/cart");
-// const Order = require('../models/order');
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
 	//replaced fetchAll with findAll sequelize method
@@ -140,9 +140,23 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-	let fetchedCart;
 	req.user
-		.addOrder()
+		.populate("cart.items.productId")
+		// .execPopulate()
+		.then((user) => {
+			const products = user.cart.items.map((i) => {
+				return { quantity: i.quantity, product: i.productId };
+			});
+			const order = new Order({
+				user: {
+					name: req.user.name,
+					userId: req.user,
+				},
+				products: products,
+			});
+			return order.save();
+		})
+
 		// .getCart()
 		// .then((cart) => {
 		// 	fetchedCart = cart;
