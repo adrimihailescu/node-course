c405da66515afb84c0094d8b90ce96813e908580
adriana <adriana.mihalescu@gmail.com>
2022-05-30 16:44:43 +0100

    add how to use the session middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 // const expressHbs = require("express-handlebars");
 
 const mongoose = require("mongoose");
+const session = require("express-session");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -51,6 +52,9 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+	session({ secret: "my secret", resave: false, saveUninitialised: false })
+);
 
 //mongoDb
 app.use((req, res, next) => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,18 @@
 exports.getLogin = (req, res, next) => {
 	// console.log(req.get("Cookie"));
-	const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1];
+	// const isLoggedIn =
+	// 	req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
+	console.log(req.session.isLoggedIn);
 	res.render("auth/login", {
 		path: "/login",
 		pageTitle: "Login",
-		isAuthenticated: isLoggedIn,
+		isAuthenticated: false,
 	});
 };
 
 exports.postLogin = (req, res, next) => {
 	// req.isLoggedIn = true;
-	res.setHeader("Set-Cookie", "loggedIn=true");
+	// res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
+	req.session.isLoggedIn = true;
 	res.redirect("/");
 };
