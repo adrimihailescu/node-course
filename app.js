ac4a2681074e46a829710b3ffbf78307eebb8bf5
adriana <adriana.mihalescu@gmail.com>
2022-05-30 15:09:07 +0100

    add login form, controller, view and router

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.set("views", "views");
 //routes
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 // db.execute("SELECT * FROM products")
 // 	.then((result) => {
@@ -65,6 +66,7 @@ app.use((req, res, next) => {
 //routes
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
