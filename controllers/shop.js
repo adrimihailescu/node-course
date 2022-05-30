8305522818cd2facd0645eaf6e3aede1dbf44b7e
adriana <adriana.mihalescu@gmail.com>
2022-05-30 15:31:30 +0100

    adding the request driven login solution

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,6 +13,7 @@ exports.getProducts = (req, res, next) => {
 				prods: products,
 				pageTitle: "All Products",
 				path: "/products",
+				isAuthenticated: req.isLoggedIn,
 			});
 		})
 		.catch((err) => console.log(err));
@@ -28,6 +29,7 @@ exports.getProduct = (req, res, next) => {
 				product: product,
 				pageTitle: product.title,
 				path: "/products",
+				isAuthenticated: req.isLoggedIn,
 			});
 		})
 		.catch((err) => console.log(err));
@@ -43,6 +45,7 @@ exports.getIndex = (req, res, next) => {
 				prods: products,
 				pageTitle: "Shop",
 				path: "/",
+				isAuthenticated: req.isLoggedIn,
 			});
 		})
 		.catch((err) => console.log(err));
@@ -61,6 +64,7 @@ exports.getCart = (req, res, next) => {
 				path: "/cart",
 				pageTitle: "Your Cart",
 				products: products,
+				isAuthenticated: req.isLoggedIn,
 			});
 		})
 		.catch((err) => console.log(err));
@@ -198,6 +202,7 @@ exports.getOrders = (req, res, next) => {
 				path: "/orders",
 				pageTitle: "Your Orders",
 				orders: orders,
+				isAuthenticated: req.isLoggedIn,
 			});
 		})
 		.catch((err) => console.log(err));
