57968db7c536edf46e395f0a4a7347c788a91b04
adriana <adriana.mihalescu@gmail.com>
2022-05-31 17:37:34 +0100

    add authentication flow and encrypting password with bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
@@ -26,7 +28,34 @@ exports.postLogin = (req, res, next) => {
 		.catch((err) => console.log(err));
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	const confirmPassword = req.body.confirmPassword;
+	User.findOne({ email: email })
+		//check if the user exist
+		.then((userDoc) => {
+			if (userDoc) {
+				return res.redirect("/signup");
+			}
+			return bcrypt.hash(password, 12);
+		})
+		.then((hashedPassword) => {
+			//create new user
+			const user = new User({
+				email: email,
+				password: hashedPassword,
+				cart: { items: [] },
+			});
+			return user.save();
+		})
+		.then((result) => {
+			res.redirect("/login");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
 
 exports.postLogout = (req, res, next) => {
 	req.session.destroy((err) => {
