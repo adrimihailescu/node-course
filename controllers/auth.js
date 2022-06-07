964df32329522e63432b89fb9f9d2fb6989ed528
adriana <adriana.mihalescu@gmail.com>
2022-06-07 18:06:15 +0100

    adding code for keeping the user input

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,6 +42,11 @@ exports.getSignup = (req, res, next) => {
 		path: "/signup",
 		pageTitle: "Signup",
 		errorMessage: message,
+		oldInput: {
+			email: "",
+			password: "",
+			confimPassword: "",
+		},
 	});
 };
 
@@ -87,6 +92,11 @@ exports.postSignup = (req, res, next) => {
 			path: "/signup",
 			pageTitle: "Signup",
 			errorMessage: errors.array()[0].msg,
+			oldInput: {
+				email: email,
+				password: password,
+				confimPassword: req.body.confimPassword,
+			},
 		});
 	}
 	bcrypt
