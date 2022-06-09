8d72167d4404581c3bf874f5e96fd6c2a704b617
adriana <adriana.mihalescu@gmail.com>
2022-06-09 15:58:16 +0100

    add validation to edit form

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,6 +14,7 @@ exports.getAddProduct = (req, res, next) => {
 		editing: false,
 		hasError: false,
 		errorMessage: null,
+		validationErrors: [],
 
 		// isAuthenticated: req.session.isLoggedIn,
 	});
@@ -41,6 +42,7 @@ exports.postAddProduct = (req, res, next) => {
 				description: description,
 			},
 			errorMessage: errors.array()[0].msg,
+			validationErrors: errors.array(),
 		});
 	}
 	const product = new Product(
@@ -104,6 +106,7 @@ exports.getEditProduct = (req, res, next) => {
 				product: product,
 				hasError: false,
 				errorMessage: null,
+				validationErrors: [],
 			});
 		})
 		.catch((err) => {
@@ -118,6 +121,26 @@ exports.postEditProduct = (req, res, next) => {
 	const updatedPrice = req.body.price;
 	const updatedDesc = req.body.description;
 	const updatedImageUrl = req.body.imageUrl;
+
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		// console.log(errors.array());
+		return res.status(422).render("admin/edit-product", {
+			pageTitle: "Edit Product",
+			path: "/admin/edit-product",
+			editing: true,
+			hasError: true,
+			product: {
+				title: updatedTitle,
+				imageUrl: updatedImageUrl,
+				price: updatedPrice,
+				description: updatedDesc,
+				_id: prodId,
+			},
+			errorMessage: errors.array()[0].msg,
+			validationErrors: errors.array(),
+		});
+	}
 
 	// const product = new Product(
 	// 	updatedTitle,
