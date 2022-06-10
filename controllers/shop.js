ea58ba07073548a7d4997198f198f91ece00918b
adriana <adriana.mihalescu@gmail.com>
2022-06-10 16:13:46 +0100

    add downloading file logic with authentication

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const Product = require("../models/product");
 // const Cart = require("../models/cart");
 const Order = require("../models/order");
@@ -241,5 +244,17 @@ exports.getCheckout = (req, res, next) => {
 	res.render("shop/checkout", {
 		path: "/checkout",
 		pageTitle: "Checkout",
+	});
+};
+
+exports.getInvoice = (req, res, next) => {
+	const orderId = req.params.orderId;
+	const invoiceName = "invoice-" + orderId + ".pdf";
+	const invoicePath = path.join("data", "invoices", invoiceName);
+	fs.readFile(invoicePath, (err, data) => {
+		if (err) {
+			return next(err);
+		}
+		res.send(data);
 	});
 };
