1859348d0f8b0dc04b61a051c942dcec87d753f4
adriana <adriana.mihalescu@gmail.com>
2022-06-14 14:45:52 +0100

    add logic for writing pdf files with order data

--- controllers/shop.js
+++ controllers/shop.js
@@ -263,6 +263,7 @@ exports.getInvoice = (req, res, next) => {
 			const invoiceName = "invoice-" + orderId + ".pdf";
 			const invoicePath = path.join("data", "invoices", invoiceName);
 
+			//version for writing pdf files directly on the server
 			const pdfDoc = new PDFDocument();
 			res.setHeader("Content-Type", "application/pdf");
 			res.setHeader(
@@ -272,7 +273,26 @@ exports.getInvoice = (req, res, next) => {
 			pdfDoc.pipe(fs.createWriteStream(invoicePath));
 			pdfDoc.pipe(res);
 
-			pdfDoc.text("Hello world!");
+			pdfDoc.fontSize(26).text("Invoice", {
+				underline: true,
+			});
+			pdfDoc.text("---------------");
+			let totalPrice = 0;
+			order.products.forEach((prod) => {
+				totalPrice += prod.quantity * prod.product.price;
+				pdfDoc
+					.fontSize(14)
+					.text(
+						prod.product.title +
+							" - " +
+							prod.quantity +
+							" x " +
+							"$" +
+							prod.product.price
+					);
+			});
+			pdfDoc.text("----");
+			pdfDoc.fontSize(20).text("Total Price: $" + totalPrice);
 
 			pdfDoc.end();
 			//for small files - preloading data
