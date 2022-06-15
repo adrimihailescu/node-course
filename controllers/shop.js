4241785e4ee2f35c14dc61c0a5e8712bc1d06615
adriana <adriana.mihalescu@gmail.com>
2022-06-15 12:31:06 +0100

    re-using pagination logic and controls

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,17 +9,29 @@ const Order = require("../models/order");
 const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
-	//replaced fetchAll with findAll sequelize method
-	//changed back to fetchAll for MongoDb
-	// Product.fetchAll()
+	const page = +req.query.page || 1;
+
+	let totalItems;
+
 	Product.find()
+		.countDocuments()
+		.then((numProducts) => {
+			totalItems = numProducts;
+			return Product.find()
+				.skip((page - 1) * ITEMS_PER_PAGE)
+				.limit(ITEMS_PER_PAGE);
+		})
 		.then((products) => {
-			console.log(products);
 			res.render("shop/product-list", {
 				prods: products,
-				pageTitle: "All Products",
+				pageTitle: "Products",
 				path: "/products",
-				// isAuthenticated: req.session.isLoggedIn,
+				currentPage: page,
+				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+				hasPreviousPage: page > 1,
+				nextPage: page + 1,
+				previousPage: page - 1,
+				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
 			});
 		})
 		.catch((err) => {
